9d9c7d2a56a858b63c5ed585c7c4829bda59c39a
Kasaksaxena <kasaksaxena6@gmail.com>
2024-10-09 21:49:08 +0530

    making final pdf templates using pandas and fpdf

--- project 4 pdf template/Main.py
+++ project 4 pdf template/Main.py
@@ -2,12 +2,12 @@ from fpdf import FPDF
 import pandas as pd
 pdf=FPDF(orientation="P", unit="mm", format="A4" )
 
-
-pdf.add_page()
-
-pdf.set_font("Arial",size=12)
-
-pdf.cell(w=0,h=12,txt="Hello There!",align="C",ln=1,border=1)
-
+df=pd.read_csv("project 4 pdf template\\topics.csv")
+for index,row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font("Times",style="B",size=25)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0,h=12,txt=row["Topic"],align="C",ln=1,border=0)
+    pdf.line(10,21,200,21)
 pdf.output("output.pdf")
 
